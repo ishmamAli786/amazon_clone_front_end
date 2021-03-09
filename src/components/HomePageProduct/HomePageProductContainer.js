2db39151e73a182d26b2e7cdc78a9a764bde7dd3
Ishmam Ali <ishmamalikhan@gmail.com>
2021-03-09 12:33:49 +0500

    home page product

---
+++ src/components/HomePageProduct/HomePageProductContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import HomePageProductView from './HomePageProductView';
+
+class HomePageContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            
+        }
+    }
+    render() {
+        return (
+            <>
+                <HomePageProductView />
+            </>
+        )
+    }
+}
+
+
+export default HomePageContainer;
---
+++ src/components/HomePageProduct/HomePageProductView.js
@@ -0,0 +1,222 @@
+import React from 'react'
+
+function HomePageProductView() {
+    return (
+        <>
+        <div className="container  mb-5" style={{marginTop: '-200px'}}>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />750.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />650.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />550.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />150.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />750.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />650.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />550.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />150.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />750.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />650.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />550.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body d-flex justify-content-center">
+                <a href="#"><img src="assets/img/product_images/produc.jpg" alt="product image" className="img-fluid" /></a>
+              </div>
+              <div className="card-footer bg-transparent py-0">
+                <div className="row my-1">
+                  <div className="col-md-6">
+                    <p className="font-weight-bold text-danger"><i className="fas fa-rupee-sign" />150.00</p>
+                  </div>
+                  <div className="col-md-6">
+                    <a href="#" className="btn btn-success  btn-block">Buy</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default HomePageProductView;
